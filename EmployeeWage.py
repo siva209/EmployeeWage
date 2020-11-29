cce23f2334eb33d69a07553eded00517c7519cb5
siva209 <sivanarayanayeruva209@gmail.com>
2020-11-29 22:45:19 +0530

    [sivanarayana]Added:Employee  is present r not

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,3 +1,16 @@
+#UC1 checking employee is present r not
 
+import random
 class EmployeeWage:
- print("Welcome to Employee Wage Computation");
+
+ def checkEmpAttendance(self):
+  attendance = random.randint(0, 1)
+  if attendance == 1:
+   return "Employee is Present"
+  else:
+   return "Employee is absent"
+
+
+if __name__ == "__main__":
+ employee = EmployeeWage()
+ print(employee.checkEmpAttendance())
