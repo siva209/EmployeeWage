d09c531df9395e7f00081bcf0a437e895aa6629d
siva209 <sivanarayanayeruva209@gmail.com>
2020-11-29 22:59:00 +0530

    [sivanarayana]Added:Employee  Daily Wages

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,16 +1,28 @@
 #UC1 checking employee is present r not
 
 import random
+import random
+
 class EmployeeWage:
+ EMP_WAGE_PER_HOUR = 20
+ employeeHours=0;
 
  def checkEmpAttendance(self):
-  attendance = random.randint(0, 1)
-  if attendance == 1:
-   return "Employee is Present"
+  empcheck = random.randint(0, 1)
+  if empcheck == 1:
+    EmployeeWage.employeeHours=8;
+    return "Employee is Present"
   else:
+   EmployeeWage.employeeHours=0
    return "Employee is absent"
+
+ def calculateDailyEmpWage(self):
+  dailyWage = EmployeeWage.EMP_WAGE_PER_HOUR * EmployeeWage.employeeHours
+  return "Employee Daily Wage is : " + str(dailyWage)
 
 
 if __name__ == "__main__":
  employee = EmployeeWage()
  print(employee.checkEmpAttendance())
+ print(employee.calculateDailyEmpWage())
+
